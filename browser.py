ae374791412ed786e31d71ccd6b1c6c4cd398b2e
YashCK <yashck2452@gmail.com>
2023-05-22 19:02:30 -0700

    Added line breaks and mousewheel support

--- browser.py
+++ browser.py
@@ -23,8 +23,12 @@ class Browser:
         self.scroll = 0
         # set up canvas
         self.canvas.pack()
-        # bind DOWN key
+        # bind keys
         self.window.bind("<Down>", self.scrolldown)
+        self.window.bind("<Up>", self.scrollup)
+        self.window.bind("<MouseWheel>", self.on_mouse_wheel)
+        self.window.bind("<Button-4>", self.mouse_scrollup)
+        self.window.bind("<Button-5>", self.mouse_scrolldown)
 
     def load(self, url: str = None):
         try:
@@ -46,16 +50,48 @@ class Browser:
         self.canvas.delete("all")
         for x, y, c in self.display_list:
             # If the characters are outside the viewing screen, skip the iteration
-            if y > self.scroll + HEIGHT: # below viewing window
+            if y > self.scroll + HEIGHT:  # below viewing window
                 continue
-            if y + VSTEP < self.scroll: # above viewing window
+            if y + VSTEP < self.scroll:  # above viewing window
                 continue
             # Otherwise add the character to the canvas
             self.canvas.create_text(x, y - self.scroll, text=c)
 
+    def on_mouse_wheel(self, e):
+        if sys.platform.startswith('win'):
+            if e.delta > 0:
+                self.mouse_scrolldown(e)
+            else:
+                self.mouse_scrollup(e)
+        elif sys.platform.startswith('darwin'):
+            if e.delta < 0:
+                self.mouse_scrolldown(e)
+            else:
+                self.mouse_scrollup(e)
+
+    def mouse_scrolldown(self, e):
+        self.scroll += SCROLL_STEP/4
+        self.draw()
+
+    def mouse_scrollup(self, e):
+        if self.scroll > 0:
+            if self.scroll - SCROLL_STEP < 0:
+                self.scroll = 0
+            else:
+                self.scroll -= SCROLL_STEP/3
+            self.draw()
+
     def scrolldown(self, e):
         self.scroll += SCROLL_STEP
         self.draw()
+
+    def scrollup(self, e):
+        if self.scroll > 0:
+            if self.scroll - SCROLL_STEP < 0:
+                self.scroll = 0
+            else:
+                self.scroll -= SCROLL_STEP
+            self.draw()
 
 
 # compute and store the position of each character
@@ -63,6 +99,12 @@ def layout(text: str) -> list[(int, int, str)]:
     display_list = []
     cursor_x, cursor_y = HSTEP, VSTEP
     for c in text:
+        # create line break for \n characters
+        if c == "\n":
+            cursor_y += 1.25 * VSTEP
+            cursor_x = HSTEP
+            continue
+        # append position and character to the display list
         display_list.append((cursor_x, cursor_y, c))
         cursor_x += HSTEP
         if cursor_x >= WIDTH - HSTEP:
@@ -74,5 +116,6 @@ def layout(text: str) -> list[(int, int, str)]:
 # Main method
 if __name__ == "__main__":
     import sys
+
     Browser().load(sys.argv[1])
     tkinter.mainloop()
